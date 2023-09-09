312642347beb91174e9191256ecc1d593df88ad7
bianca9901 <biancawikberg99@yahoo.com>
2023-09-09 09:00:30 +0200

    Add view and serializer and make custom permissions

---
+++ drf_api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+class IsOwnerOnly(permissions.BasePermission):
+    """
+    Custom permission that checks wheter the requesting user is
+    the owner of the object being accessed. It allows the owner
+    to perform read and write operations on the object while
+    denying access to others.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+#from drf_api.permissions import IsAuthenticated
+from .models import Profile
+from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOnly
 
-# Create your views here.
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """ Retrieve & Update a user's profile.
+    This allows a user to perform these operations on their
+    own profile. Note, this makes users private and not visible
+    to other users. Will update these permissions if I have time to 
+    make user2user interactions a feauture.
+    """
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+    permission_classes = [IsOwnerOnly]
