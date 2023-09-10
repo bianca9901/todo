c659754c534675c9fc2554773a8dcbf3303cfba1
bianca9901 <biancawikberg99@yahoo.com>
2023-09-10 12:46:07 +0200

    Add views to habit app and add filtering and ordering fields to task and habit

--- habit/views.py
+++ habit/views.py
@@ -1,19 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Habit
 from .serializers import HabitSerializer
 from drf_api.permissions import IsOwnerOnly
 
 class HabitList(generics.ListCreateAPIView):
     serializer_class = HabitSerializer
-    queryset = Habit.objects.all()
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['frequency']
+    ordering_fields = ['frequency', 'created_at']
     permission_classes = [IsAuthenticated, IsOwnerOnly]
+    
+    def get_queryset(self):
+        queryset = Habit.objects.filter(owner=self.request.user)
+        frequency = self.request.query_params.get('frequency', None)
+        if frequency:
+            queryset = queryset.filter(frequency=frequency)
+        return queryset
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
-    def get_queryset(self):
-        return Habit.objects.filter(owner=self.request.user)
 
 class HabitDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = HabitSerializer
--- task/views.py
+++ task/views.py
@@ -1,19 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 from drf_api.permissions import IsOwnerOnly
 
 class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['priority']
+    ordering_fields = ['priority', 'created_at']
     permission_classes = [IsAuthenticated, IsOwnerOnly]
+    
+    def get_queryset(self):
+        queryset = Task.objects.filter(owner=self.request.user)
+        priority = self.request.query_params.get('priority', None)
+        if priority:
+            queryset = queryset.filter(priority=priority)
+        return queryset
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
-    def get_queryset(self):
-        return Task.objects.filter(owner=self.request.user)
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
