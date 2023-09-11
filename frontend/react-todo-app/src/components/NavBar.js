60555344d2fa509aac1f96770c16c36650b6f95d
bianca9901 <biancawikberg99@yahoo.com>
2023-09-11 16:40:06 +0200

    Add CSS to NavBar and change logo to transparent

--- frontend/react-todo-app/src/components/NavBar.js
+++ frontend/react-todo-app/src/components/NavBar.js
@@ -2,25 +2,27 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-            <img src={logo} alt="logo" height="150">
-            </img>
+          <img src={logo} alt="logo" height="150"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>
-                <i className="fas fa-sign-in-alt"></i>Sign in
-                </Nav.Link>
-            <Nav.Link>
-            <i className="fas fa-user-plus"></i>Sign in
+            <Nav.Link className={styles.NavLink}>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink}>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink}>
+              <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
