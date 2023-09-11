53fbe6a1a78af3806607d6fe751accaa28a4e340
bianca9901 <biancawikberg99@yahoo.com>
2023-09-11 17:47:48 +0200

    Add React Router Dom and implemented navbar routes

--- frontend/react-todo-app/src/components/NavBar.js
+++ frontend/react-todo-app/src/components/NavBar.js
@@ -4,26 +4,42 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="150"></img>
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="150"></img>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link className={styles.NavLink}>
+            <NavLink
+              exact
+              to="/"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
               <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link className={styles.NavLink}>
+            </NavLink>
+            <NavLink
+              to="/signin"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
               <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link className={styles.NavLink}>
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
               <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
