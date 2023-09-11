549fff70c4aeee4d0721f03f610337885057485e
bianca9901 <biancawikberg99@yahoo.com>
2023-09-11 06:54:09 +0200

    Add pagination and date & time preferences

--- task/models.py
+++ task/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -7,7 +8,7 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(default=timezone.now)
     completed = models.BooleanField(default=False)
     priority = models.CharField(
         max_length=10,
--- task/serializers.py
+++ task/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    due_date_formatted = serializers.SerializerMethodField()
+    due_date_natural = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_due_date_natural(self, obj):
+        return naturaltime(obj.due_date)
+    
+    def get_due_date_formatted(self, obj):
+        return obj.due_date.strftime('%d %b %Y %H:%M')
     class Meta:
         model = Task
         fields = ['id', 'owner', 'created_at', 'updated_at', 'title',
-                  'description', 'due_date', 'completed', 'priority',
+                  'description', 'due_date_formatted', 'due_date_natural', 'completed', 'priority',
                   'category',]
         
         read_only_fields = ['owner']
