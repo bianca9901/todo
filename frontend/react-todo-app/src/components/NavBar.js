7c94904c27fa03daa45b3b23b5d2c425f0d5eb7d
bianca9901 <biancawikberg99@yahoo.com>
2023-09-12 16:10:05 +0200

    Add icons to navbar and implement custom hook
    -Add logout and profile icons.
    -Implement custom toggle hook.

--- frontend/react-todo-app/src/components/NavBar.js
+++ frontend/react-todo-app/src/components/NavBar.js
@@ -5,12 +5,43 @@ import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
-  const currentUser = useCurrentUser()
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+ const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loggedInIcons = (
+    <>
+      <NavLink to="/" onClick={handleSignOut} className={styles.NavLink}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+
+      <NavLink
+        to={`/profiles/${currentUser?.profile_id}`}
+        onClick={() => {}}
+        className={styles.NavLink}
+      >
+        <i className="fa-regular fa-user"></i>Profile
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -31,14 +62,18 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="150"></img>
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <NavLink
