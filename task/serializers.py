31ca967ef35986ec0976d602fbe3dafe1c668a30
bianca9901 <biancawikberg99@yahoo.com>
2023-09-14 11:35:46 +0200

    Fix folder structure in pages

--- task/serializers.py
+++ task/serializers.py
@@ -4,10 +4,15 @@ from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
     
     class Meta:
         model = Task
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'title',
+        fields = ['id', 'owner', 'is_owner', 'created_at', 'updated_at', 'title',
                   'description', 'due_date', 'completed', 'priority',
                   'category',]
         
