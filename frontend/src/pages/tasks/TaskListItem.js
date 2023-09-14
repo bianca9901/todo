5287e52c14fc17cec69d105183c27ee903be0e85
bianca9901 <biancawikberg99@yahoo.com>
2023-09-14 18:03:33 +0200

    Add Bootstrap fade to toggle task details

--- frontend/src/pages/tasks/TaskListItem.js
+++ frontend/src/pages/tasks/TaskListItem.js
@@ -1,41 +1,40 @@
-import React from "react";
-import { ListGroup, Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { ListGroup, Button, Fade } from "react-bootstrap";
 import styles from "../../styles/TaskListItem.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 function TaskListItem({ task }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h2 className={styles.TaskDetailHeader}>Task Details</h2>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <strong>Title:</strong> {task.title}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Due Date:</strong> {task.due_date}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Priority:</strong> {task.priority}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Category:</strong> {task.category}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Description:</strong> {task.description}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Completed:</strong>{" "}
-              {task.completed ? "Yes" : "No"}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <ListGroup.Item>
+        <strong>Title:</strong> {task.title}
+        <br />
+        <strong>Due Date:</strong> {task.due_date}
+        <br />
+        <Button
+          className={`${btnStyles.Button} ${btnStyles.Bright}`}
+          onClick={() => setOpen(!open)}
+          aria-controls={`task-details-${task.id}`}
+          aria-expanded={open}
+        >
+          Show Details
+        </Button>
+        <Fade in={open}>
+          <div id={`task-details-${task.id}`}>
+            <strong>Priority:</strong> {task.priority}
+            <br />
+            <strong>Category:</strong> {task.category}
+            <br />
+            <strong>Description:</strong> {task.description}
+            <br />
+            <strong>Completed:</strong> {task.completed ? "Yes" : "No"}
+          </div>
+        </Fade>
+      </ListGroup.Item>
+    </>
   );
 }
 
 export default TaskListItem;
-
-
-
