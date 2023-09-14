8dfaae77b920a72b63cc25e7f48cb0bba0553f93
bianca9901 <biancawikberg99@yahoo.com>
2023-09-14 08:16:03 +0200

    Delete date formatting from models
    Will use the frontend to adapt to Django ISO time and to calculate "Days and Hours left" to make backend cleaner.

--- task/models.py
+++ task/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,14 +7,13 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    due_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField()
     completed = models.BooleanField(default=False)
     priority = models.CharField(
         max_length=10,
         choices=[("Low", "Low"), ("Medium", "Medium"), ("High", "High")]
     )
     category = models.CharField(max_length=50)
-    
 
     def __str__(self):
         return self.title
