1bb28c452fce2a6ff2f94b40140b38101d2c0703
bianca9901 <biancawikberg99@yahoo.com>
2023-09-23 13:38:20 +0200

    Add loading spinner to my profile and add bootstrap styling

--- frontend/src/pages/profile/MyProfile.js
+++ frontend/src/pages/profile/MyProfile.js
@@ -5,6 +5,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import cartoons from "../../assets/cartoons.png";
 import styles from "../../styles/MyProfile.module.css";
 import NotAuthenticated from "../../components/NotAuthenticated";
+import Asset from "../../components/Asset";
 
 /*
  * My Profile (Component):
@@ -18,6 +19,7 @@ import NotAuthenticated from "../../components/NotAuthenticated";
 const MyProfile = () => {
   const [profile, setProfile] = useState(null); //profile data intially set to null
   const currentUser = useCurrentUser(); //get the currently logged in user
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (currentUser) {
@@ -26,9 +28,11 @@ const MyProfile = () => {
         .get(`/profiles/${currentUser.profile_id}/`)
         .then((response) => {
           setProfile(response.data); // Update the profile state with the fetched data
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error(error);
+          setIsLoading(false);
         });
     }
   }, [currentUser]); // Trigger this effect when currentUser changes
@@ -36,38 +40,33 @@ const MyProfile = () => {
   return (
     <>
       {currentUser ? (
-        <Container
-          className="d-flex flex-column justify-content-center
-    align-items-center"
-        >
+        <Container className="d-flex flex-column justify-content-center align-items-center">
           <Card className={styles.Card}>
-            {currentUser ? (
-              <div>
-                <Card.Header className={`${styles.Header} py-5`}>
-                  Welcome to your profile, {currentUser.username}!{" "}
-                </Card.Header>
-                {profile ? (
-                  <>
-                    <Card.Text className="py-3">
+            <div>
+              <Card.Header className={`${styles.Header} py-5`}>
+                Welcome to your profile, {currentUser.username}!
+              </Card.Header>
+              {isLoading ? ( // Display spinner when loading
+                <Asset spinner message="Loading your profile..." />
+              ) : profile ? (
+                <>
+                  <Card.Text className="py-3">
+                    <strong>
                       You have been staying organized since {profile.created_at}
-                    </Card.Text>
-                    <Card.Footer>
-                      <img
-                        src={cartoons}
-                        alt="cartoons doing tasks"
-                        height="150"
-                      />
-                    </Card.Footer>
-                  </>
-                ) : null}
-              </div>
-            ) : (
-              <p>Loading your profile...</p>
-            )}
+                    </strong>
+                  </Card.Text>
+                </>
+              ) : null}
+              <Card.Footer className="d-flex flex-column align-items-center">
+                <img src={cartoons} alt="cartoons doing tasks" height="150" />
+                <Card.Subtitle className="mb-2 mt-4 text-muted">
+                  Exciting new features are on the way! while you wait, we are
+                  here to help you stay organized and wish you the best of luck
+                  with your tasks. You are doing well!
+                </Card.Subtitle>
+              </Card.Footer>
+            </div>
           </Card>
-          ...Exciting new features are on the way! while you wait, we are here
-          to help you stay organized and wish you the best of luck with your
-          tasks. You are doing well!
         </Container>
       ) : (
         <NotAuthenticated />
