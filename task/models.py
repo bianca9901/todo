c67eb168e8d1939c80f99079d83d75c2e7c0045c
bianca9901 <biancawikberg99@yahoo.com>
2023-09-23 21:29:48 +0200

    Add model for notes
    - Removed model task statistics due to time constraints

--- task/models.py
+++ task/models.py
@@ -18,3 +18,11 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+class Note(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Note by {self.owner.username}"
