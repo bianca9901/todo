cedad2b39764ea17c136ac703d3f0f8258d773bd
bianca9901 <biancawikberg99@yahoo.com>
2023-09-24 12:47:23 +0200

    Add 'NoTasks' component to display to users without tasks

---
+++ frontend/src/components/NoTasks.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, Container } from "react-bootstrap";
+import styles from "../styles/NoTasks.module.css";
+
+/*
+ * No Tasks (Component):
+ * This component is displayed when the user don't have any tasks.
+ *
+ */
+
+const NoTasks = () => {
+  return (
+    <Container className="d-flex justify-content-center align-items-center">
+      <Card className={styles.Card}>
+        <Card.Header className={styles.Header}>
+          No Tasks <i className="fa-solid fa-magnifying-glass"></i>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>Your task list is empty</Card.Title>
+          <Card.Text>
+            Start creating tasks to stay organized and achieve your goals!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default NoTasks;
