aeb239e7359d26b5456c5d6bbb8471fad76268f1
bianca9901 <biancawikberg99@yahoo.com>
2023-09-24 12:02:42 +0200

    Change multi-line comments to single-line for clarity

--- task/models.py
+++ task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 class Task(models.Model):
-    """ Represents a task associated with a user """
+    # Represents a task associated with a user
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,7 +22,7 @@ class Task(models.Model):
 
 
 class Note(models.Model):
-    """ Represents a note associated with a user """
+    # Represents a note associated with a user
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
