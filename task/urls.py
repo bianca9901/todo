08bf483258f704e80e5a9760c9945131dc011167
bianca9901 <biancawikberg99@yahoo.com>
2023-09-24 08:43:24 +0200

    Add url patterns for notes

--- task/urls.py
+++ task/urls.py
@@ -4,4 +4,6 @@ from task import views
 urlpatterns = [
     path('tasks/', views.TaskList.as_view()),
     path('task/<int:pk>/', views.TaskDetail.as_view()),
+    path('notes/', views.NoteList.as_view()),
+    path('note/<int:pk>/', views.NoteDetail.as_view()),
 ]
