3d02f9d098ea0f030232f246630eed86639148ee
bianca9901 <biancawikberg99@yahoo.com>
2023-09-24 18:02:10 +0200

    Add 'Note', 'Not-Authenticated' and 'NoTasks' components to 'Tasksall' page
    - Add improved comment to describe the flow
    - Fix long lines of code to be on two lines

--- frontend/src/pages/tasks/TasksAll.js
+++ frontend/src/pages/tasks/TasksAll.js
@@ -1,37 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
-import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { Row, Col, ListGroup } from "react-bootstrap";
 import TaskListItem from "../../components/TaskListItem";
 import NavTask from "../../components/NavTask";
 import NoResultsError from "../../components/NoResultsError";
 import Asset from "../../components/Asset";
+import Note from "../../components/Note";
+import NotAuthenticated from "../../components/NotAuthenticated";
+import NoTasks from "../../components/NoTasks";
 
 /*
  * TasksAll (Parent Component):
- * This component manages the list of all tasks, including fetching, deleting,
- * marking as completed, searching for tasks and filtering/ordering tasks and more.
- *
- * Responsibilities:
- * - Fetches and displays the list of all tasks with a default display
- *   of tasks ordered by creation date in descending order.
- * - Handles task deletion.
- * - Handles marking tasks as completed.
- * - Enables filtering tasks based on search.
- * - Enables ordering tasks based on user selected options.
- * - Enables filtering tasks based on selected priority.
+ * This component manages the complete task list, handling actions like 
+ * fetching, deleting, and marking tasks as completed.
+ * 
+ * TasksAll has two direct child components, NavTask and TaskListItem, to which
+ * it passes necessary props. While most logic is modularized within these child
+ * components, API calls are managed by TasksAll, the parent.
+ * 
+ * - NavTask: Renders the task-related navigation bar, including the search
+ *   input field and dropdown menus for filtering and ordering.
+ * 
+ * - TaskListItem: Renders individual task items with buttons to perform actions
+ *   such as delete, mark as completed, and edit.
+ * 
+ * Additionally, TasksAll utilizes three more components:
+ * 
+ * - "Note" is rendered beside the tasks always.
+ * 
+ * - "No Results Error" is rendered when no search results are found.
+ * 
+ * - "Not Authenticated" is rendered when the user is not authenticated.
  */
 
 function TasksAll() {
   const [tasks, setTasks] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [orderBy, setOrderBy] = useState("-created_at"); // Default order of tasks
+  const [orderBy, setOrderBy] = useState("-created_at"); // Default order of
+  //tasks
   const [selectedPriority, setSelectedPriority] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const currentUser = useCurrentUser();
 
   useEffect(() => {
-    // Fetches all tasks when component mounts
+    // Fetches all tasks when component mounts or when
+    // orderBy or selectedPriority changes
     const fetchTasks = async () => {
       try {
         const response = await axiosReq.get(
@@ -84,38 +98,49 @@ function TasksAll() {
   });
 
   return (
-    <Container>
-      <NavTask // Props to child Nav-Task
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-        orderBy={orderBy}
-        setOrderBy={setOrderBy}
-        selectedPriority={selectedPriority}
-        setSelectedPriority={setSelectedPriority}
-      />
-      <Row className="mt-4">
-        <Col>
-        {isLoading ? ( // Displays a spinner when tasks are being fetched
-            <Asset spinner message="Loading your tasks..." />
-          ) : (
-          <ListGroup>
-            {filteredTasks.length > 0 ? (
-              filteredTasks.map((task) => (
-                <TaskListItem // Props to child Task-List-Item
-                  key={task.id}
-                  task={task}
-                  onDelete={onDelete}
-                  onMarkAsCompleted={markAsCompleted}
-                />
-              ))
-            ) : (
-              <NoResultsError /> // No results found component
-            )}
-          </ListGroup>
-          )}
-        </Col>  
-      </Row>
-    </Container>
+    <>
+      {currentUser ? (
+        <>
+          <NavTask // Props to child Nav-Task
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+            orderBy={orderBy}
+            setOrderBy={setOrderBy}
+            selectedPriority={selectedPriority}
+            setSelectedPriority={setSelectedPriority}
+          />
+          <Row className="mt-4">
+            <Col>
+              {isLoading ? ( // Displays a spinner when tasks are being fetched
+                <Asset spinner message="Loading your tasks..." />
+                ) : tasks.length === 0 ? (
+                  <NoTasks/> // Task list is empty component
+              ) : (
+                <ListGroup>
+                  {filteredTasks.length > 0 ? (
+                    filteredTasks.map((task) => (
+                      <TaskListItem // Props to child Task-List-Item
+                        key={task.id}
+                        task={task}
+                        onDelete={onDelete}
+                        onMarkAsCompleted={markAsCompleted}
+                      />
+                    ))
+                  ) : (
+                    <NoResultsError /> // No results found component
+                  )}
+                </ListGroup>
+              )}
+            </Col>
+            <Col>
+              <Note /> {/* Note Component */}
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <NotAuthenticated /> // Not an authenticated user component
+      )}
+    </>
   );
 }
 
