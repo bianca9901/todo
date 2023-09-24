f6764060f71910082fe5395fe261e1fd62dfa915
bianca9901 <biancawikberg99@yahoo.com>
2023-09-24 12:11:01 +0200

    Fix grammar mistake in comment and pep8 secure

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -1,12 +1,11 @@
 from rest_framework import permissions
 
+
 class IsOwnerOnly(permissions.BasePermission):
     """
-    Custom permission that checks wheter the requesting user is
-    the owner of the object being accessed. It allows the owner
-    to perform read and write operations on the object while
-    denying access to others.
+    Custom permission that checks whether the requesting user is the owner
+    of the object being accessed. This permission allows the owner to perform
+    read and write operations on the object while denying access to others.
     """
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
-    
